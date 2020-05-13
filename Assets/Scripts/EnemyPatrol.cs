b1cfea678d0acaf1626fc18ddba9da867bb4a519
Panda <dulka1503@onet.pl>
2020-05-13 17:50:09 +0200

    Added enemy patrol movement and health bar sprite

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed;
+    private bool movingLeft = true;
+    public Transform edgeDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        RaycastHit2D edgeInfo = Physics2D.Raycast(edgeDetection.position,Vector2.down,2.0f);
+        if(edgeInfo.collider==false)
+        {
+            if(movingLeft==true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = true;
+            }
+        }
+    }
+}
